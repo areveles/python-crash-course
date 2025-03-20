11c41826bbe40dd5e46d846a429a76566cdc5154
Alex Reveles <areveles@student.umgc.edu>
2025-03-19 19:19:01 -0700

    Refactored code

--- day1_basics/day1_basics_exercises.py
+++ day1_basics/day1_basics_exercises.py
@@ -8,17 +8,20 @@
 # Exercise 1: Sum, difference, product, quotient
 x = 8
 y = 3
-print( x + y)
-print(x - y)
-print(x * y)
-print(x / y)
+print("Exercise 1: Sum, difference, product, quotient")
+print(f"Sum: {x + y}")
+print(f"Difference: {x - y}")
+print(f"Product: {x * y}")
+print(f"Quotient: {x / y}\n")
 
 # Exercise 2: Name and color
+print("Exercise 2: Name and Color")
 name = input("Please enter your name: \n")
 color = input("Please enter your favorite color: \n")
-print(f'Hi {name}! Your favorite color is {color}')
+print(f'Hi {name}! Your favorite color is {color}\n')
 
 # Exercise 3: Product of two numbers
+print("Exercise 3: Product of two numbers")
 user_input = input("Enter two numbers, separated by a space: ")
 num1, num2 = user_input.split()
 
@@ -27,9 +30,10 @@ num2 = int(num2)
 
 product = num1 * num2
 
-print(f"The product of {num1} and {num2} is {product}")
+print(f"The product of {num1} and {num2} is {product}\n")
 
 # Exercise 4: Remainder
+print("Exercise 4: Remainder")
 num1 = int(input("Please enter a number: "))
 num2 = int(input("Please enter a second number: "))
 result = num1 % num2

f715dfcf792e807bd07bdd9153d78f357f2770a2
Alex Reveles <areveles@student.umgc.edu>
2025-03-22 14:54:29 -0700

    Finished Day 3 exercises

--- day3_functions/exercises.py
+++ day3_functions/exercises.py
@@ -10,25 +10,40 @@ Day 3 - Functions Exercises
 
 # Exercise 1
 def add_numbers(a, b):
-    pass
+    return f"The sum of {a} and {b} is {a+b}"
 
 # Exercise 2
 def even_or_odd(num):
-    pass
+    if num % 2 == 0:
+        return f"{num} is even"
+    else:
+        return f"{num} is odd"
 
 # Exercise 3
 def greet(name, color="blue"):
-    pass
+    return f"Hello, {name}! Your favorite color is {color}"
 
 # Exercise 4
 def factorial(n):
-    pass
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
 # Exercise 5
 def find_largest(numbers):
-    pass
+    largest = numbers[0]
+    for num in numbers:
+        if num > largest:
+            largest = num
+    return largest
 
 # Optional: Example calls here if you want to test
 if __name__ == "__main__":
     # Example: print(add_numbers(3,4))
-    pass
+    print(add_numbers(3,4))
+    print(even_or_odd(3))
+    print(greet("Alex","red"))
+    print(greet("Alex"))
+    print(factorial(5))
+    print(find_largest([3,9,2,15,6]))
